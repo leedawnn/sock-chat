40b7cf11950b35c47552d6f093fff0bb80cc7228
leedawnn <2g_hee@naver.com>
2021-10-07 16:38:26 +0900

    feat: Add indexRouter

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get((req, res, next) => {
+  return res.send("indexRouter works!");
+});
+
+module.exports = router;
