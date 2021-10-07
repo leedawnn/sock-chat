88ae65c003047242da9e198ea8c1bd540efbeda7
leedawnn <2g_hee@naver.com>
2021-10-07 16:53:00 +0900

    feat: Add clubsRouter

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const http = require('http');
 const server = http.createServer(app);
 
 
-app.get('/', (req, res) => {
-    res.send('Express work!');
-});
 const indexRouter = require('./routes/index');
+const clubsRouter = require('./routes/clubs')
 
 app.use('/', indexRouter);
+app.use('/clubs', clubsRouter);
 
 server.listen(3000, () => {
     console.log('Express Server is now listening on localhost:3000..');
