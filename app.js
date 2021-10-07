44db2234fbd55508ca2814ff173ce0c7d01c8f2c
leedawnn <2g_hee@naver.com>
2021-10-07 16:40:09 +0900

    feat: Add indexRouter

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-const indexRouter = require('./routes/idnex');
+
+app.get('/', (req, res) => {
+    res.send('Express work!');
+});
+const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
 server.listen(3000, () => {
-  console.log('Express Server is now listening on localhost:3000..');
+    console.log('Express Server is now listening on localhost:3000..');
 });
